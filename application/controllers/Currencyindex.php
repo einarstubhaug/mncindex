c5080a98e56f8ead7e692f052a6db01349093455
mncindex.com <mncindex.com@onecom.(none)>
2016-01-06 15:22:51 +0000

    Renaming controllers according to codeigniter conventinos

--- application/controllers/Currencyindex.php
+++ application/controllers/Currencyindex.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class CurrencyIndex extends CI_Controller {
+class Currencyindex extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
