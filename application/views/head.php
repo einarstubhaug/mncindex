bd33954ade94d014ab47cecdb1bdf13de61e0cd6
Einar Stubhaug <einar@ravn.no>
2016-03-06 10:28:08 +0100

    New version of jqplot

--- application/views/head.php
+++ application/views/head.php
@@ -1,4 +1,4 @@
-<head>
+d<head>
     <!--link rel="stylesheet" type="text/css" href="default.css"-->
     <!--script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script-->
     <script type="text/javascript" src="/assets/js/jqplot/src/jquery.js"></script>
@@ -6,7 +6,7 @@
 
         <!-- Latest compiled and minified JavaScript -->
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script><!-- Latest compiled and minified CSS -->
-      
+
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
 
     <!-- Optional theme -->
@@ -16,16 +16,16 @@
     <link rel="stylesheet" href="/assets/css/default.css">
     <script type="text/javascript" src="/assets/js/thirdparty/angular1-5-0.min.js"></script>
 
-    <script type="text/javascript" src="/assets/js/jqplot/src/jquery.jqplot.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.canvasTextRenderer.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.canvasAxisLabelRenderer.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.canvasAxisTickRenderer.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.dateAxisRenderer.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.cursor.js"></script>
-    <script type="text/javascript" src="/assets/js/jqplot/src/plugins/jqplot.highlighter.js"></script>
-    
-    <link rel="stylesheet" type="text/css" href="/assets/js/jqplot/src/jquery.jqplot.css" />
 
+    <script src="/assets/js/thirdparty/jquery.jqplot.min.js" ></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasTextRenderer.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasAxisLabelRenderer.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasAxisTickRenderer.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.dateAxisRenderer.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.cursor.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.highlighter.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="/assets/css/thirdparty/jquery.jqplot.min.css" />
     <script type="text/javascript" src="/assets/js/mncindex.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
