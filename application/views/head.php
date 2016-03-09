3458035f003691d20d5b3cb558df8098e2ff0168
Einar Stubhaug <einar@ravn.no>
2016-03-09 21:05:26 +0100

    Added Grunt to project and bundled jqplot

--- application/views/head.php
+++ application/views/head.php
@@ -1,6 +1,6 @@
 <head>
-    <!--link rel="stylesheet" type="text/css" href="default.css"-->
-    <!--script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script-->
+
+    <meta charset='utf-8'/> 
     <script type="text/javascript" src="/assets/js/thirdparty/jquery-1.12.1.min.js"></script>
     <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 
@@ -16,22 +16,22 @@
     <link rel="stylesheet" href="/assets/css/default.css">
     <script type="text/javascript" src="/assets/js/thirdparty/angular1-5-0.min.js"></script>
 
-
-    <script src="/assets/js/thirdparty/jquery.jqplot.min.js" ></script>
+    <script src="/assets/js/thirdparty/jqplot-bundle.min.js"></script>
+    <!-- jqueryplot and the plugins we use -->
+    <!--script src="/assets/js/thirdparty/jquery.jqplot.min.js" ></script>
     <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasTextRenderer.js"></script>
     <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasAxisLabelRenderer.js"></script>
     <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.canvasAxisTickRenderer.js"></script>
     <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.dateAxisRenderer.js"></script>
     <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.cursor.js"></script>
-    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.highlighter.js"></script>
+    <script type="text/javascript" src="/assets/js/thirdparty/jqplot-plugins/jqplot.highlighter.js"></script-->
 
     <link rel="stylesheet" type="text/css" href="/assets/css/thirdparty/jquery.jqplot.min.css" />
+    
     <script type="text/javascript" src="/assets/js/mncindex.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="/assets/js/jquery.marquee.min.js"></script>
-
-    <meta charset='utf-8'/> 
 
     <script src="/assets/js/mncindex_app/app.js"></script>
     <script src="/assets/js/mncindex_app/controllers/currencycalculator.js"></script>
